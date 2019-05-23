65980cff60ccc9a1f1f57e104122134a6f817802
Jose Vega <josevega234@gmail.com>
2019-05-23 18:56:26 +0200

    Version Beta No esta como me gustaria pero hay que entregar He tenido que quitar funcionalidades de la pagina para poder entregar a tiempo. Good Luck

--- src/js/main.js
+++ src/js/main.js
@@ -5,19 +5,27 @@ const dir = "http://api.openweathermap.org/data/2.5/weather?q=Getafe&units=metri
  */
 
 function getWeather() {
-    var request = new XMLHttpRequest();
+    let request = new XMLHttpRequest();
     request.onreadystatechange = function () {
-        if(this.readyState == 4 && this.status == 200){
+        if (this.readyState == 4 && this.status == 200) {
             var response = request.responseXML;
             var temp = parseTemp(response);
             temp.value += " ºC";
             var _temp = document.getElementById('temperatura');
             _temp.innerHTML = temp.value;
-            
+
         }
     }
-    request.open("GET", dir, true);
+    request.open("POST", dir, true); /* he usado POST porque va la api-key */ 
     request.send();
+}
+
+function getMain(){
+    return '<div class="title"><h1>Como llego a ...</h1></div><form action="" method="get" class="d-flex" onsubmit="return getResponse()"><input type="search" class="form-control form-control-lg" name="buscar" id="busca_form"><input type="submit" class="btn btn-primary btn-lg buscaBtn" value="Buscar"></form>';
+}
+
+function getFooter(){
+    return '<div class="d-flex justify-content-center align-items-center"><p>Creado por Jose Vega con licencia Creative Commons w/ share alike</p></div>';
 }
 
 function parseTemp(xml) {
@@ -27,98 +35,78 @@ function parseTemp(xml) {
     return temp;
 }
 
+function getJSON() {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        let objeto = JSON.parse(this.responseText);
+        let origen = objeto.routes[0].legs[0].start_address;
+        let destino = objeto.routes[0].legs[0].end_address;
+        let steps = objeto.routes[0].legs[0].steps;
+        muestraOrigen(origen);
+        muestraDestino(destino);
+        muestraLosPasos(steps);
+    }
+    request.open("GET", "response-data-export.json", true);
+    request.send();
+}
+
+function muestraOrigen(origen) {
+    let card = '<div class="card w-75 border-primary"><div class="card-header bg-secondary text-white">Origen</div>';
+    card += '<div class="card-body"><h5 class="card-title">'
+    card += origen;
+    card += '</h5></div></div>';
+    document.getElementById('resultOrigen').innerHTML = card;
+}
+
+function muestraDestino(destino) {
+    let card = '<div class="card w-75 border-primary"><div class="card-header bg-secondary text-white">Destino</div>';
+    card += '<div class="card-body"><h5 class="card-title">'
+    card += destino;
+    card += '</h5></div></div>';
+    document.getElementById('resultDestino').innerHTML = card;
+}
+
+function creaTarjetaDePaso(numero, paso){
+    let card = '<div class="card border-primary mt-4 w-75"><div class="card-header bg-info text-white">Paso ';
+    card += numero;
+    card += '</div>';
+    card += '<div class="card-body"><h5 class="card-title">';
+    card += paso.html_instructions;
+    card += '</h5></div></div>';
+    return card;
+}
+
+function getResponse(){
+    alert('no te enfades, pero te voy dar las indicaciones para ir al estadio Metropolitano como ejemplo');
+    let detail = document.getElementById('main');
+    detail.innerHTML = "";
+    detail.innerHTML = scaffoldResponse();
+    getJSON();
+}
+
+function scaffoldResponse(){
+    return '<section class="results d-flex flex-column justify-content-center"><div id="resultOrigen" class="d-flex justify-content-center mt-4"></div><div id="resultDestino" class="d-flex justify-content-center mt-4 mb-4"></div><div id="resultPasos"></div></section';
+}
+ 
+function muestraLosPasos(steps) {
+    let htmlCards = "";
+    for (let j = 0; j < steps.length; j++) {
+        htmlCards += creaTarjetaDePaso(j+1, steps[j]);
+    }
+    document.getElementById('resultPasos').innerHTML = htmlCards;
+}
 
 window.onload = function () {
     document.getElementById('ciudad').innerHTML = "Getafe";
     getWeather();
+    document.getElementById('main').innerHTML = getMain();
+
+    /*He tenido que quitar el footer porque para que el footer se quede en 
+    su sitio dinamicamente debo cargar el contenido del padre dinamicamente (que en este caso seria body).
+    He intentado cargarlo dinamicamente pero no se coloca en su sitio, y no me queda tiempo 
+    para poder arreglarlo */
+
+    //document.getElementById('footer').innerHTML = getFooter();
 }
 
-/**
- * esta es la funcion que se dispara con el metodo onsubmit del
- * formulario
- */
 
-function buscaRuta(){
-    var reqText = document.getElementById('busca_form').value;
-    var consulta = sanitize(reqText);
-    getSelfLocation(consulta);
-}
-
-/**
- * esta funcion transforma la cadena "calle moncloa madrid" en "calle+moncloa+madrid"
- * @param req 
- */
-
-function sanitize(req){
-    var reqArr = req.split(' ');
-    var res = "";
-    for (let index = 0; index < reqArr.length; index++) {
-        const element = reqArr[index];
-        res += element;
-        if(index < (reqArr.length -1)){
-            res+= "+";
-        }
-    }
-    return res;
-}
-
-/**
- * obtener mi geolocalizacion a traves del API del navegador
- * @param {*} destino que se le pasamos por parametro
- */
-
-function getSelfLocation(destino){
-    /* mockeado para depuracion
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(peticion(destino));
-    }else{
-        alert('Tu navegador no es compatible con la geolocalizacion');
-    }
-    */
-    peticion(destino);
-}
-/*
-function peticion(destino, position){
-    let origenLat = position.coords.latitude;
-    let origenLon = position.coords.longitude;
-    let req = "https://maps.googleapis.com/maps/api/directions/json?origin=";
-    req+= origenLat + "," + origenLon;
-    req+= "&destination=" + destino;
-    req+= "&mode=transit&language=es&key=AIzaSyBYsR-WKuJSziP1WysyPuc-bzyzQHESDxA";
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            let response = request.responseText;
-            parseResponse(response);
-        }        
-    }
-    request.open("POST", req, true);
-    request.send();
-}
-mockeado para depuracion
-*/
-
-/**
- * el origen esta mockeado para depurar el codigo
- */
-function peticion(destino){
-    let req = "https://maps.googleapis.com/maps/api/directions/json?origin=";
-    req+= "calle+madrid+100+getafe";
-    req+= "&destination=" + destino;
-    req+= "&mode=transit&language=es&key=AIzaSyBYsR-WKuJSziP1WysyPuc-bzyzQHESDxA";
-    let reqdos = new XMLHttpRequest();
-    reqdos.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            let response = request.responseText;
-            console.log(response);
-            parseResponse(response);
-        }        
-    }
-    reqdos.open("GET", req, true);
-    reqdos.send();
-}
-
-function parseResponse(response) {
-    let resp = JSON.parse(response);
-    console.log(resp);
-}
